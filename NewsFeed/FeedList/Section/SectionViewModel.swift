1b0a8449818c8e663a7566dce1db560fbf6d5073
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-06-16 14:14:52 +0200

    Section added

---
+++ NewsFeed/FeedList/Section/SectionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SectionViewModel.swift
+//  NewsFeed
+//
+//  Created by Mustafa Ozhan on 16/06/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import Foundation
+struct SectionViewModel {
+    let rowViewModels: [RowViewModel]
+    let headerTitle: String
+}
